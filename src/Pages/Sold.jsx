ac865d37eac73271ccfe90dc108eb8227e432347
kailas1246 <kailasgrtvm@gmail.com>
2025-06-27 21:33:51 +0530

    Update Sold.jsx

--- src/Pages/Sold.jsx
+++ src/Pages/Sold.jsx
@@ -281,7 +281,7 @@ const sold = () => {
                     <tbody>
                         {selectedProduct && (
                             <div
-                                className="fixed left-[70%] top-[100px] transform -translate-x-1/2 -translate-y-1/2 bg-white text-black p-6 rounded-xl shadow-2xl border-2 border-red-700 z-50 product-detail-popup"
+                                className="fixed left-[95%] top-[100px] transform -translate-x-1/2 -translate-y-1/2 bg-white text-black p-6 rounded-xl shadow-2xl border-2 border-red-700 z-50 product-detail-popup"
                             >
                                 <div className="flex justify-between items-center">
 
@@ -394,9 +394,7 @@ const sold = () => {
                                     required
                                 >
                                     <option value="">Select Unit</option>
-                                    <option value="KG">KG</option>
-                                    <option value="Litre">Litre</option>
-                                    <option value="Piece">Piece</option>
+                                    <option value="Default">Default</option>
                                 </select>
                             </div>
 
